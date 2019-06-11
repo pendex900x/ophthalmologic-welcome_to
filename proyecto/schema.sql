222d2e6638501dc4289104385adca53636c0bd99
pendex900x <36184284+pendex900x@users.noreply.github.com>
2019-06-11 17:49:50 -0400

    Update schema.sql

--- proyecto/schema.sql
+++ proyecto/schema.sql
@@ -1,106 +1,386 @@
-/*
-* Sistema control de citas WELCOME Database
-* @author localhost
-*/
-create database bookmedik;
-use bookmedik;
-set sql_mode='';
-create table user (
-	id int not null auto_increment primary key,
-	username varchar(50),
-	name varchar(50),
-	lastname varchar(50),
-	email varchar(255),
-	password varchar(60),
-	is_active boolean not null default 1,
-	is_admin boolean not null default 0,
-	created_at datetime
-);
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 11-06-2019 a las 17:49:15
+-- Versión del servidor: 5.7.17-log
+-- Versión de PHP: 5.6.30
 
-insert into user (username,password,is_admin,is_active,created_at) value ("admin",sha1(md5("admin")),1,1,NOW());
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-create table pacient (
-	id int not null auto_increment primary key,
-	no varchar(50),
-	name varchar(50),
-	lastname varchar(50),
-	gender varchar(1),
-	day_of_birth date,
-	email varchar(255),
-	address varchar(255),
-	phone varchar(255),
-	image varchar(255),
-	sick varchar(500),
-	medicaments varchar(500),
-	alergy varchar(500),
-	is_favorite boolean not null default 1,
-	is_active boolean not null default 1,
-	created_at datetime
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table category (
-	id int not null auto_increment primary key,
-	name varchar(200)
-	);
+--
+-- Base de datos: `welcometo`
+--
 
-insert into category (name) value ("Modulo 1");
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `category`
+--
 
-create table medic (
-	id int not null auto_increment primary key,
-	no varchar(50),
-	name varchar(50),
-	lastname varchar(50),
-	gender varchar(1),
-	day_of_birth date,
-	email varchar(255),
-	address varchar(255),
-	phone varchar(255),
-	image varchar(255),
-	is_active boolean not null default 1,
-	created_at datetime,
-	category_id int,
-	foreign key (category_id) references category(id)
-);
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `category`
+--
 
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Modulo 1');
 
-create table status (
-	id int not null auto_increment primary key,
-	name varchar(100)
-);
+-- --------------------------------------------------------
 
-insert into status (id,name) values (1,"Pendiente"), (2,"Aplicada"),(3,"No asistio"),(4,"Cancelada");
+--
+-- Estructura de tabla para la tabla `instrument`
+--
 
-create table payment (
-	id int not null auto_increment primary key,
-	name varchar(100)
-);
+CREATE TABLE `instrument` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-insert into payment (id,name) values  (1,"Pendiente"),(2,"Pagado"),(3,"Anulado");
+--
+-- Volcado de datos para la tabla `instrument`
+--
 
-create table reservation(
-	id int not null auto_increment primary key,
-	title varchar(100),
-	note text,
-	message text,
-	date_at varchar(50),
-	time_at varchar(50),
-	created_at datetime,
-	pacient_id int,
-	symtoms text,
-	sick text,
-	medicaments text,
-	user_id int,
-	medic_id int,
-	price double,
-	is_web boolean not null default 0,
-	payment_id int not null default 1,
-	foreign key (payment_id) references payment(id),
-	status_id int not null default 1,
-	foreign key (status_id) references status(id),
-	foreign key (user_id) references user(id),
-	foreign key (pacient_id) references pacient(id),
-	foreign key (medic_id) references medic(id)
-);
+INSERT INTO `instrument` (`id`, `name`, `cantidad`, `created_at`) VALUES
+(6, 'wena', 3, '2019-05-01 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medic`
+--
+
+CREATE TABLE `medic` (
+  `id` int(11) NOT NULL,
+  `no` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `gender` varchar(1) DEFAULT NULL,
+  `day_of_birth` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `medic`
+--
+
+INSERT INTO `medic` (`id`, `no`, `name`, `lastname`, `gender`, `day_of_birth`, `email`, `address`, `phone`, `image`, `is_active`, `created_at`, `category_id`) VALUES
+(1, NULL, 'doctor bacan', 'gano mucha money', NULL, NULL, 'maildeldoc', 'la dehesa', 'telefonodeldoc', NULL, 1, '2019-04-27 00:12:48', 1),
+(2, NULL, 'medico millonario 23', 'wena', NULL, NULL, 'mail', 'direccc', 'tell', NULL, 1, '2019-04-27 00:15:05', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `instrument_1` int(11) DEFAULT NULL,
+  `instrument_2` int(11) DEFAULT NULL,
+  `instrument_3` int(11) DEFAULT NULL,
+  `instrument_4` int(11) DEFAULT NULL,
+  `instrument_5` int(11) DEFAULT NULL,
+  `id_reserva` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacient`
+--
+
+CREATE TABLE `pacient` (
+  `id` int(11) NOT NULL,
+  `no` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `gender` varchar(1) DEFAULT NULL,
+  `day_of_birth` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `sick` varchar(500) DEFAULT NULL,
+  `medicaments` varchar(500) DEFAULT NULL,
+  `alergy` varchar(500) DEFAULT NULL,
+  `is_favorite` tinyint(1) NOT NULL DEFAULT '1',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pacient`
+--
+
+INSERT INTO `pacient` (`id`, `no`, `name`, `lastname`, `gender`, `day_of_birth`, `email`, `address`, `phone`, `image`, `sick`, `medicaments`, `alergy`, `is_favorite`, `is_active`, `created_at`) VALUES
+(14, '195227993', 'asdasd', 'asdasd', 'h', '0000-00-00', 'asd', 'asdasd', 'asd', NULL, '', '', '', 1, 1, '2019-06-09 22:11:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `payment`
+--
+
+INSERT INTO `payment` (`id`, `name`) VALUES
+(1, 'Pendiente'),
+(2, 'Pagado'),
+(3, 'Anulado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `note` text,
+  `message` text,
+  `date_at` varchar(50) DEFAULT NULL,
+  `time_at` varchar(50) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `pacient_id` int(11) DEFAULT NULL,
+  `symtoms` text,
+  `sick` text,
+  `medicaments` text,
+  `user_id` int(11) DEFAULT NULL,
+  `medic_id` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `is_web` tinyint(1) NOT NULL DEFAULT '0',
+  `payment_id` int(11) NOT NULL DEFAULT '1',
+  `status_id` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'Pendiente'),
+(2, 'Aplicada'),
+(3, 'No asistio'),
+(4, 'Cancelada');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `is_contador` tinyint(1) NOT NULL,
+  `is_inventario` tinyint(1) NOT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `name`, `lastname`, `email`, `password`, `is_active`, `is_admin`, `is_contador`, `is_inventario`, `created_at`) VALUES
+(1, 'admin', 'admin', 'admin', NULL, '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', 1, 1, 0, 0, '2019-04-27 00:08:51'),
+(2, 'noadmin', 'no es admin', 'no es admin', NULL, 'd9670b53a148b51c0187a8e9ac66a81ab9a138de', 1, 0, 1, 1, '2019-04-27 00:20:19');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `instrument`
+--
+ALTER TABLE `instrument`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `medic`
+--
+ALTER TABLE `medic`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indices de la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_reserva` (`id_reserva`),
+  ADD KEY `instrument_1` (`instrument_1`),
+  ADD KEY `instrument_2` (`instrument_2`),
+  ADD KEY `instrument_3` (`instrument_3`),
+  ADD KEY `instrument_4` (`instrument_4`),
+  ADD KEY `instrument_5` (`instrument_5`);
+
+--
+-- Indices de la tabla `pacient`
+--
+ALTER TABLE `pacient`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `payment_id` (`payment_id`),
+  ADD KEY `status_id` (`status_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pacient_id` (`pacient_id`),
+  ADD KEY `medic_id` (`medic_id`);
+
+--
+-- Indices de la tabla `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `instrument`
+--
+ALTER TABLE `instrument`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `medic`
+--
+ALTER TABLE `medic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pacient`
+--
+ALTER TABLE `pacient`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT de la tabla `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `medic`
+--
+ALTER TABLE `medic`
+  ADD CONSTRAINT `medic_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Filtros para la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `id_instrument_1` FOREIGN KEY (`instrument_1`) REFERENCES `instrument` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_instrument_2` FOREIGN KEY (`instrument_2`) REFERENCES `instrument` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_instrument_3` FOREIGN KEY (`instrument_3`) REFERENCES `instrument` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_instrument_4` FOREIGN KEY (`instrument_4`) REFERENCES `instrument` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_instrument_5` FOREIGN KEY (`instrument_5`) REFERENCES `instrument` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_reservation` FOREIGN KEY (`id_reserva`) REFERENCES `reservation` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`id`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`),
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `reservation_ibfk_4` FOREIGN KEY (`pacient_id`) REFERENCES `pacient` (`id`),
+  ADD CONSTRAINT `reservation_ibfk_5` FOREIGN KEY (`medic_id`) REFERENCES `medic` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
